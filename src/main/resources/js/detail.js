140c13d15b273fba0030d1b0727f88175eb9bc20
Johnnyy91 <claudiosardilli@gmail.com>
2023-02-27 17:46:00 +0100

    add create

--- src/main/resources/js/detail.js
+++ src/main/resources/js/detail.js
@@ -4,11 +4,11 @@ const pizzaId = URLParams.get('id');
 axios.get(`http://localhost:8080/api/${pizzaId}`).then((res) => {
     console.log('richiesta ok', res);
   
-    
+
     document.querySelector('#name').innerHTML = res.data.name;
     document.querySelector('#description').innerHTML = res.data.description;
     document.querySelector('#price').innerHTML = res.data.price;
-    document.querySelector('#imgPath').innerHTML = res.data.imgPath;
+    document.querySelector('#imgPath').innerHTML = res.data.imgPath ;
 
    
 }).catch((res) => {

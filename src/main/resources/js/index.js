6339868c05ce61ac86791b5f8353063e328a2854
Johnnyy91 <claudiosardilli@gmail.com>
2023-02-27 16:49:12 +0100

    add index

--- src/main/resources/js/index.js
+++ src/main/resources/js/index.js
@@ -1,2 +1,32 @@
 console.log('HELLO JS');
 
+pizzaList();
+
+function pizzaList() {
+    axios.get('http://localhost:8080/api').then((res) => {
+
+        document.querySelector('#pizza_table').innerHTML = '';
+        res.data.forEach(pizza => {
+            console.log(pizza);
+            document.querySelector('#pizza_table').innerHTML += `
+            <tr>
+                <td>
+                    <a href="./detail.html?id=${pizza.id}">${pizza.id}</a>
+                </td>
+                <td>${pizza.name}</td>
+                <td>${pizza.description}</td>
+                <td>${pizza.price}</td>    
+                <td>
+                    <a class="btn btn-primary" href="detail.html?id=${pizza.id}">
+                        <i class="fa-solid fa-pen-to-square"> </i>
+                    </a>
+                </td>         
+            </tr>
+            `
+        });
+    }).catch((res) => {
+    
+        console.error('errore nella richiesta', res);
+        alert('Errore durante la richiesta!');
+    })
+}

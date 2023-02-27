8fbe6568caf87ba31ae39ac755a9178333f09fef
Johnnyy91 <claudiosardilli@gmail.com>
2023-02-27 17:54:24 +0100

    add imPath

--- src/main/resources/js/detail.js
+++ src/main/resources/js/detail.js
@@ -8,7 +8,7 @@ axios.get(`http://localhost:8080/api/${pizzaId}`).then((res) => {
     document.querySelector('#name').innerHTML = res.data.name;
     document.querySelector('#description').innerHTML = res.data.description;
     document.querySelector('#price').innerHTML = res.data.price;
-    document.querySelector('#imgPath').innerHTML = res.data.imgPath ;
+    document.querySelector('#imgPath').innerHTML = '<img src="' + res.data.imgPath + '" width=300px />' ;
 
    
 }).catch((res) => {
